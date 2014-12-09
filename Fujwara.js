96253c4f4c30608823a53e5d9bac106d1d2fc5a1
藤原彰吾 <fujiwarashougo@fujiwarashougo-no-MacBook-Air.local>
2014-12-09 21:42:37 +0900

    kousinn

--- Fujwara.js
+++ Fujwara.js
@@ -1,4 +1,4 @@
-var decide(y, d, m) = function(){
+function decide(y, d, m){
 	var numY=y;
 	numY=parselnt(numY);
 	numYY=Math.floor((numY%100)/4);
@@ -9,32 +9,32 @@ var decide(y, d, m) = function(){
 	var numM=m;
 	numM=parselnt(numM);
 
-	var second = Math.floor((26*(numM+1))/10)
+	var second = Math.floor((26*(numM+1))/10);
 
 	var numJ = -2*(numY%100)+Math.floor((numY%100)/4);
 
 	var sum=(numD+second+numY%100+numYY+numJ)%7;
 
 	switch(sum){
-	case 0;
+	case 0:
 	alert('sat');
 	 break;
-	case 1;
+	case 1:
 	alert('sun');
 	 break;
-	case 2;
+	case 2:
 	alert('mon');
 	 break;
-	case 3;
+	case 3:
 	alert('tue');
 	 break;
-	case 4;
+	case 4:
 	alert('wed');
 	 break;
-	case 5;
+	case 5:
 	alert('thu');
 	break;
-	case 6;
+	case 6:
 	alert('fri');
 	 break;
 	}
